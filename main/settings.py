3ec72f5ea8440a9d8bd59c82a3cfb5586958971e
Dan Goriaynov <dan.goriaynov@gmail.com>
2015-11-08 23:01:11 +0200

    made mock page opened by default

--- main/settings.py
+++ main/settings.py
@@ -64,7 +64,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': ['static/templates',],
-        'APP_DIRS': True,
+        #'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
--- market/urls.py
+++ market/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 from market import views
 from market.views import MainView
 
 urlpatterns = [
     #url(r'^r/$', views.post_results, name='results'),
-    url(r'^$', MainView.as_view(), name='index'),
+    url(r'^$', TemplateView.as_view(template_name='market/mock_page.html'),
+    name='index'),
 ]
